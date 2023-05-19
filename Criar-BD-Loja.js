a446f4aded27d8b1d08b431cf6e1be344275bf44
Bruno Paulo <bpauloec@gmail.com>
2023-05-19 12:50:56 -0300

    DB connection and Product List method

--- Criar-BD-Loja.js
+++ Criar-BD-Loja.js
@@ -7,42 +7,42 @@ db.Produto.insertMany( [
     idProduto: 1,
     nome: 'Celular Galaxy S10',
     descricao: '128 Gb, Preto, com carregador',
-    valor: 1250,
+    valor: 1250.00,
     estado: 'Poucos riscos, estado de novo'
    },
   {
     idProduto: 2,
     nome: 'Prod2',
     descricao: 'Bla bla',
-    valor: 1100,
+    valor: 1100.00,
     estado: 'Bla bla'
    },
   {
     idProduto: 3,
     nome: 'Prod3',
     descricao: 'Bla bla',
-    valor: 120,
+    valor: 120.00,
     estado: 'Bla bla'
    },
   {
     idProduto: 4,
     nome: 'Prod4',
     descricao: 'Bla bla',
-    valor: 1300,
+    valor: 1300.00,
     estado: 'Bla bla'
    },
   {
     idProduto: 5,
     nome: 'Prod5',
     descricao: 'Bla bla',
-    valor: 9400,
+    valor: 9400.00,
     estado: 'Bla bla'
    },
   {
     idProduto: 6,
     nome: 'Prod6',
     descricao: 'Bla bla',
-    valor: 1600,
+    valor: 1600.00,
     estado: 'Bla bla'
    },
 ] )
--- src/main/java/br/unicamp/ic/inf335/Loja.java
+++ src/main/java/br/unicamp/ic/inf335/Loja.java
@@ -1,10 +1,70 @@
 package br.unicamp.ic.inf335;
 
+import org.bson.Document;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 public class Loja {
 
+	/***
+	 * Conectar com Banco de Dados MongoDB
+	 * @param endereco - Endereço para conexão mongodb://[nome do servidor]
+	 * @param nomeBD - Nome do Banco de Dados
+	 * @return Objeto de Banco de Dados MongoDB
+	 */
+	public MongoDatabase conecta(String endereco, String nomeBD){
+		MongoClient client = null;
+		MongoDatabase db = null;
+		
+		try{
+			client = MongoClients.create(endereco);
+			db = 	client.getDatabase(nomeBD);
+		}
+		catch(Exception e){
+			System.out.println("Erro de conexão e selecionar banco: " + e);
+			e.printStackTrace();
+		}
+		
+		return db;
+	}
+	
+	/***
+	 * Listar Produtos do banco de dados Loja
+	 * @param db - Objeto de Banco de Dados MongoDB
+	 */
+	public void listaProdutos(MongoDatabase db){
+		try{
+			MongoCollection<Document> collection = db.getCollection("Produto");
+			
+			Iterable<Document> produtos = collection.find();
+			
+			System.out.println("ID Produto\tNome\tDescricao\tValor(reias)\tEstado");
+			for(Document produto : produtos) {
+				int idProduto = produto.getInteger("idProduto");
+				String nome = produto.getString("nome");
+				String descricao = produto.getString("descricao");
+				int valor = produto.getInteger("valor");
+				String estado = produto.getString("estado");
+				
+				System.out.println(idProduto + "\t" + nome + "\t" + descricao + "\t" + valor + "\t" + estado);
+			}
+		}
+		catch (Exception e){
+			System.out.println("Erro ao Listar Produtos: " + e);
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Loja loja = new Loja();
+		
+		MongoDatabase lojaDB = loja.conecta("mongodb://localhost", "Loja");
+		
+		loja.listaProdutos(lojaDB);
+		System.out.println();
 	}
 
 }
